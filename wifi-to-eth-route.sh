833ec5de348a7bac8f2252b1cbfef241ad3cfaed
Arpit Agarwal <arpitjindal97@gmail.com>
2018-02-07 00:57:08 +0530

    Removed unnecessary route

--- wifi-to-eth-route.sh
+++ wifi-to-eth-route.sh
@@ -30,15 +30,17 @@ sudo sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
 
 sudo ifconfig $eth $ip_address netmask $netmask
 
-sudo ip route del 0/0 dev $eth &> /dev/null
-a=`route | awk "/${wlan}/"'{print $5+1;exit}'`
-sudo route add -net default gw $ip_address netmask 0.0.0.0 dev $eth metric $a
+sudo systemctl stop dnsmasq
 
-echo -e "interface=$eth \n\
-bind-interfaces \n\
-server=8.8.8.8 \n\
-domain-needed \n\
-bogus-priv \n\
+echo -e "interface=$eth\n\
+bind-interfaces\n\
+server=8.8.8.8\n\
+domain-needed\n\
+bogus-priv\n\
 dhcp-range=$dhcp_range_start,$dhcp_range_end,$dhcp_time" > /etc/dnsmasq.conf
+
+# This file will work if resolveconf package is installed
+# Due to new updates in dnsmasq
+cp /etc/dnsmasq.conf /run/dnsmasq/resolv.conf
 
 sudo systemctl start dnsmasq
