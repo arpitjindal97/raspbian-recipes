f957b2e7a885129ddcbbc1fc3d310fc60396cb22
Arpit Agarwal <arpitjindal1997@hotmail.com>
2018-02-23 21:59:23 +0530

    Added readable echo statements

--- wifi-to-eth-bridge.sh
+++ wifi-to-eth-bridge.sh
@@ -30,28 +30,39 @@ sudo iptables -t nat -F
 
 sudo sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
 
-sudo killall parprouted
+echo "Killing parprouted "
+sudo killall parprouted &> /dev/null
+echo "Stopping dhcp-helper service"
 sudo systemctl stop dhcp-helper
-sudo killall dhcp-helper
+echo "Killing dhcp-helper"
+sudo killall dhcp-helper &> /dev/null
+echo "Stopping avahi-daemon service"
 sudo systemctl stop avahi-daemon
-sudo killall avahi-daemon
+echo "Killing avahi-daemon"
+sudo killall avahi-daemon &> /dev/null
 
 option=$(cat /etc/avahi/avahi-daemon.conf | grep enable-reflector=yes)
 
+echo "Creating temp avahi conf file"
 cp /etc/avahi/avahi-daemon.conf /tmp/avahi-daemon.conf
 
 if [ $option=="" ]; then
 	echo -e '\n[reflector]\nenable-reflector=yes\n' >> /tmp/avahi-daemon.conf
 fi
 
+echo "Starting parprouted ..."
 sudo /usr/sbin/parprouted $eth $wlan &
 
+echo "Starting dhcp-helper ..."
 sudo /usr/sbin/dhcp-helper -r /var/run/dhcp-helper.pid -b $wlan &
 
+echo "Flushing $eth IP addr"
 sudo ip addr flush dev $eth
 
+echo "Assigning IP to $eth from $wlan"
 sudo /sbin/ip addr add $(/sbin/ip addr show $wlan | perl -wne 'm|^\s+inet (.*)/| && print $1')/32 dev $eth &
 
 sleep 2
 
+echo "Starting avahi-daemon ... "
 avahi-daemon -f /tmp/avahi-daemon.conf &
